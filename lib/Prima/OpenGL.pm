5b92eb57c247991dbde4df19f923a5503ebb56fa
Dmitry Karasik <dmitry@karasik.eu.org>
2011-07-27 09:32:52 +0200

    - v0.03

--- lib/Prima/OpenGL.pm
+++ lib/Prima/OpenGL.pm
@@ -10,7 +10,7 @@ use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
 
 sub dl_load_flags { 0x01 };
 
-$VERSION = '0.02';
+$VERSION = '0.03';
 @EXPORT = qw();
 @EXPORT_OK = qw();
 %EXPORT_TAGS = ();
