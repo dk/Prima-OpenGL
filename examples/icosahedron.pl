844ad6b700d8d4c4e3199e47a54d1d16009418cc
Dmitry Karasik <dmitry@karasik.eu.org>
2016-11-05 16:26:52 +0100

    minor fix

--- examples/icosahedron.pl
+++ examples/icosahedron.pl
@@ -121,6 +121,7 @@ sub display
 	glRotatef($config-> {spin},0,1,0);
 	if ( $config-> {grab} ) {
 		my ( $x, $y ) = $config-> {widget}-> pointerPos;
+		$y = -$y;
 		glRotatef( $x, 0, 1, 0);
 		glRotatef( $y, 1, 0, 0);
 	}		
