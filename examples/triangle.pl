68adab5ea5800dc12bac8f30eaeea94fd206a92e
Dmitry Karasik <dmitry@karasik.eu.org>
2017-01-01 14:29:18 +0100

    clear alpha background

--- examples/triangle.pl
+++ examples/triangle.pl
@@ -7,6 +7,8 @@ $::application-> insert( GLWidget =>
 	size    => [400,400],
 	layered => 1,
 	onPaint => sub {
+		glClearColor(0,0,0,0);
+		glClear(GL_COLOR_BUFFER_BIT);
 		glBegin(GL_TRIANGLES);
 		glColor3f(1.0,0.0,0.0);
 		glVertex3f( 0.0, 1.0, 0.0);
