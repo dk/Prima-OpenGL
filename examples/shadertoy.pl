a7935531ef8bfedb63e496396829e5308041e65b
Dmitry Karasik <dmka@novozymes.com>
2017-06-26 14:54:47 +0200

    remove vbo stuff

--- examples/shadertoy.pl
+++ examples/shadertoy.pl
@@ -16,7 +16,7 @@ use OpenGL::Modern::Helpers qw(
 );
 use Prima qw(Application GLWidget);
 
-my (%uniforms, %shaders, $shader_text, $program, $vbo_quad);
+my (%uniforms, %shaders, $shader_text, $program);
 my ($gl_initialized, $fullscreen, $xres, $yres, $time, $state, $frames);
 my ( $window, $gl_widget);
 my $started      = time;
@@ -42,26 +42,11 @@ HEADER
 
 my $fragment_footer = <<'FRAGMENT_FOOTER';
 void main() {
-	vec4 color = vec4(1.0,0.0,0.0,1.0);
+	vec4 color = vec4(0.0,0.0,0.0,1.0);
 	mainImage( color, gl_FragCoord.xy );
 	gl_FragColor = color;
 }
 FRAGMENT_FOOTER
-
-my $default_fragment_shader = <<'FRAGMENT';
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-	vec2 uv = fragCoord.xy / iResolution.xy;
-	fragColor = vec4(uv,0.5+0.5*sin(iGlobalTime),cos(iGlobalTime));
-}
-FRAGMENT
-	
-my $default_vertex_shader = <<'VERTEX';
-attribute vec2 pos;
-void main() {
-	gl_Position = vec4(pos,0.0,1.0);
-}
-VERTEX
 
 sub create_shader
 {
@@ -85,11 +70,7 @@ sub create_shader
 
 sub init_shader
 {
-#	create_shader( GL_VERTEX_SHADER, $default_vertex_shader );
-	create_shader( GL_FRAGMENT_SHADER, 
-		$fragment_header . 
-		($shader_text // $default_fragment_shader). 
-		$fragment_footer );
+	create_shader( GL_FRAGMENT_SHADER, $fragment_header .  $shader_text .  $fragment_footer );
 
 	$program = glCreateProgram;
 	die "Couldn't create shader program: " . glGetError . "\n" unless $program;
@@ -121,56 +102,19 @@ sub init_shader
 	}
 }
 
-# We want static memory here
-# A 2x2 flat-screen set of coordinates for the triangles
-my @vertices = ( 
-	-1.0, -1.0,   1.0, -1.0,    -1.0,  1.0,
-	1.0, -1.0,   1.0,  1.0,    -1.0,  1.0
-);
-my $vertices = pack_GLfloat(@vertices);
-
-# create a 2D quad Vertex Buffer
-sub create_unit_quad
-{
-	glGenVertexArrays_c( 1, iv_ptr( my $buffer, 8 ));
-	my $vao = (unpack 'I', $buffer)[0];
-	glBindVertexArray($vao);
-	glObjectLabel_c(GL_VERTEX_ARRAY, $vao, length("myVAO"), "myVAO");
-	croak_on_gl_error;
-	
-	glGenBuffers_c( 1, iv_ptr($buffer, 8));
-	croak_on_gl_error;
-	$vbo_quad = (unpack 'I', $buffer)[0];
-	
-	glBindBuffer( GL_ARRAY_BUFFER, $vbo_quad );
-	glBufferData_c(GL_ARRAY_BUFFER, length($vertices), iv_ptr($vertices), GL_DYNAMIC_DRAW);
-	glObjectLabel_c(GL_BUFFER, $vbo_quad, length("my triangles"), "my triangles");
-	croak_on_gl_error;
-}
-
-sub use_quad
-{
-	my $vpos = glGetAttribLocation_c($program, 'pos');
-        die "Couldn't get shader attribute 'pos'\n" unless $vpos;
-	glEnableVertexAttribArray( $vpos );
-	glVertexAttribPointer_c( $vpos, 2, GL_FLOAT, GL_FALSE, 0, 0 );
-	glBindBuffer(GL_ARRAY_BUFFER, $vbo_quad);
-}
-
 sub update_uniforms
 {
 	$time = time - $started;
-	glProgramUniform1f( $program, $uniforms{iGlobalTime}, $time)             if $uniforms{iGlobalTime};
-	glProgramUniform3f( $program, $uniforms{iResolution}, $xres, $yres, 1.0) if $uniforms{iResolution};
-	if ( $state->{grab} && $uniforms{iMouse} ) {
-		#my $iMouse = pack_GLfloat($gl_widget->pointerPos,0,0);
-		my ($x, $y) = $gl_widget->pointerPos;
-		#glProgramUniform4fv_c( $program, $uniforms{"iMouse"}, length($iMouse) / (4*4), iv_ptr($iMouse));
-		glProgramUniform4f( $program, $uniforms{iMouse},$x,$y,0,0) if $uniforms{iMouse};
+	glProgramUniform1f( $program, $uniforms{iGlobalTime}, $time)             if defined $uniforms{iGlobalTime};
+	glProgramUniform3f( $program, $uniforms{iResolution}, $xres, $yres, 1.0) if defined $uniforms{iResolution};
+	if ( $state->{grab} && defined $uniforms{iMouse} ) {
+		my $iMouse = pack_GLfloat($gl_widget->pointerPos,0,0);
+		glProgramUniform4fv_c( $program, $uniforms{"iMouse"}, length($iMouse) / (4*4), iv_ptr($iMouse));
 	}
 }
 
 $window = Prima::MainWindow->create(
+	text => 'Shader toy',
 	size => [ 640, 480 ],
 	menuItems => [['~Options' => [
 		[ ( $fullscreen ? '*' : '') . 'fullscreen', '~Fullscreen', 'Alt+Enter', km::Alt|kb::Enter, sub {
@@ -212,49 +156,47 @@ sub create_gl_widget
 	} else {
 		%param = (
 			growMode   => gm::Client,
-			rect       => [0, $window->font->height + 4, $window->width, $window->height],
+			rect       => [0, 0, $window->width, $window->height],
 		);
 	}
 
 	$gl_widget = $window->insert( GLWidget =>
-		pack    => { expand => 1, fill => 'both'},
+		%param,
 		onPaint => sub {
-		my $self = shift;
+			my $self = shift;
+	
+			unless( $gl_initialized ) {
+				my $err = OpenGL::Modern::glewInit;
+				die "Couldn't initialize Glew: ".glewGetErrorString($err) unless $err == GLEW_OK;
+				$gl_initialized = 1;
+	                }
+			init_shader unless $program;
 
-		unless( $gl_initialized ) {
-			my $err = OpenGL::Modern::glewInit;
-			die "Couldn't initialize Glew: ".glewGetErrorString($err) unless $err == GLEW_OK;
-			init_shader;
-                	create_unit_quad;
-			$gl_initialized = 1;
-                }
-			
-		use_quad;
-
-		glClearColor(0,0,0,1);
-		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-		
-		glUseProgram( $program );
-		update_uniforms;
-		glBegin(GL_POLYGON);
-			glVertex2f(-1,-1);
-			glVertex2f(-1, 1);
-			glVertex2f( 1, 1);
-			glVertex2f( 1,-1);
-		glEnd();
-		glUseProgram( 0 );
-		glFlush;
-        },
-        onMouseDown  => sub { $state->{grab} = 1 },
-        onMouseUp    => sub { $state->{grab} = 0 },
-        onSize       => sub { ( $xres,$yres ) = shift->size },
-        onClose      => sub {
-		glUseProgram(0);
-		glDetachShader( $program, $_ ) for values %shaders;
-		glDeleteProgram( $program );
-		glDeleteShader( $_ ) for values %shaders;
-        },
+			glUseProgram( $program );
+			update_uniforms;
+			glBegin(GL_POLYGON);
+				glVertex2f(-1,-1);
+				glVertex2f(-1, 1);
+				glVertex2f( 1, 1);
+				glVertex2f( 1,-1);
+			glEnd();
+			glUseProgram( 0 );
+			glFlush;
+	        },
+	        onMouseDown  => sub { $state->{grab} = 1 },
+	        onMouseUp    => sub { $state->{grab} = 0 },
+	        onSize       => sub { ( $xres,$yres ) = shift->size },
+	        onClose      => sub {
+			glUseProgram(0);
+			glDetachShader( $program, $_ ) for values %shaders;
+			glDeleteProgram( $program );
+			glDeleteShader( $_ ) for values %shaders;
+	        },
     );
+
+    undef $program;
+    undef %uniforms;
+    undef %shaders;
 
     $gl_widget->focus if $fullscreen;
 }
@@ -262,7 +204,6 @@ sub create_gl_widget
 sub recreate_gl_widget
 {
 	$gl_widget->destroy;
-	undef $vbo_quad;
 	create_gl_widget();
 }
 
@@ -273,13 +214,206 @@ $window->insert( Timer =>
 	name    => 'Timer',
 	onTick  => sub { $gl_widget->repaint }
 )->start;
+				
 
-if ( $ARGV[0] ) {
-	local $/;
-	if ( open F, '<', $ARGV[0]) {
-		$shader_text = <F>;
-		close F;
-	}
+local $/;
+if ( $ARGV[0] && open(F, '<', $ARGV[0])) {
+	$shader_text = <F>;
+	close F;
+} else {
+	$shader_text = <DATA>;
+	close(DATA);
 }
 
 run Prima;
+
+__DATA__
+
+/*
+"Seascape" by Alexander Alekseev aka TDM - 2014
+License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+Contact: tdmaav@gmail.com
+*/
+
+const int NUM_STEPS = 8;
+const float PI	 	= 3.1415;
+const float EPSILON	= 1e-3;
+float EPSILON_NRM	= 0.1 / iResolution.x;
+
+// sea
+const int ITER_GEOMETRY = 3;
+const int ITER_FRAGMENT = 5;
+const float SEA_HEIGHT = 0.6;
+const float SEA_CHOPPY = 4.0;
+const float SEA_SPEED = 0.8;
+const float SEA_FREQ = 0.16;
+const vec3 SEA_BASE = vec3(0.1,0.19,0.22);
+const vec3 SEA_WATER_COLOR = vec3(0.8,0.9,0.6);
+float SEA_TIME = 1.0 + iGlobalTime * SEA_SPEED;
+mat2 octave_m = mat2(1.6,1.2,-1.2,1.6);
+
+// math
+mat3 fromEuler(vec3 ang) {
+	vec2 a1 = vec2(sin(ang.x),cos(ang.x));
+    vec2 a2 = vec2(sin(ang.y),cos(ang.y));
+    vec2 a3 = vec2(sin(ang.z),cos(ang.z));
+    mat3 m;
+    m[0] = vec3(a1.y*a3.y+a1.x*a2.x*a3.x,a1.y*a2.x*a3.x+a3.y*a1.x,-a2.y*a3.x);
+	m[1] = vec3(-a2.y*a1.x,a1.y*a2.y,a2.x);
+	m[2] = vec3(a3.y*a1.x*a2.x+a1.y*a3.x,a1.x*a3.x-a1.y*a3.y*a2.x,a2.y*a3.y);
+	return m;
+}
+float hash( vec2 p ) {
+float h = dot(p,vec2(127.1,311.7));	
+    return fract(sin(h)*43758.5453123);
+}
+float noise( in vec2 p ) {
+    vec2 i = floor( p );
+    vec2 f = fract( p );	
+	vec2 u = f*f*(3.0-2.0*f);
+    return -1.0+2.0*mix( mix( hash( i + vec2(0.0,0.0) ), 
+                     hash( i + vec2(1.0,0.0) ), u.x),
+                mix( hash( i + vec2(0.0,1.0) ), 
+                     hash( i + vec2(1.0,1.0) ), u.x), u.y);
+}
+
+// lighting
+float diffuse(vec3 n,vec3 l,float p) {
+    return pow(dot(n,l) * 0.4 + 0.6,p);
+}
+float specular(vec3 n,vec3 l,vec3 e,float s) {    
+    float nrm = (s + 8.0) / (3.1415 * 8.0);
+    return pow(max(dot(reflect(e,n),l),0.0),s) * nrm;
+}
+
+// sky
+vec3 getSkyColor(vec3 e) {
+    e.y = max(e.y,0.0);
+    vec3 ret;
+    ret.x = pow(1.0-e.y,2.0);
+    ret.y = 1.0-e.y;
+    ret.z = 0.6+(1.0-e.y)*0.4;
+    return ret;
+}
+
+// sea
+float sea_octave(vec2 uv, float choppy) {
+    uv += noise(uv);        
+    vec2 wv = 1.0-abs(sin(uv));
+    vec2 swv = abs(cos(uv));    
+    wv = mix(wv,swv,wv);
+    return pow(1.0-pow(wv.x * wv.y,0.65),choppy);
+}
+
+float map(vec3 p) {
+    float freq = SEA_FREQ;
+    float amp = SEA_HEIGHT;
+    float choppy = SEA_CHOPPY;
+    vec2 uv = p.xz; uv.x *= 0.75;
+    
+    float d, h = 0.0;    
+    for(int i = 0; i < ITER_GEOMETRY; i++) {        
+    	d = sea_octave((uv+SEA_TIME)*freq,choppy);
+    	d += sea_octave((uv-SEA_TIME)*freq,choppy);
+        h += d * amp;        
+    	uv *= octave_m; freq *= 1.9; amp *= 0.22;
+        choppy = mix(choppy,1.0,0.2);
+    }
+    return p.y - h;
+}
+
+float map_detailed(vec3 p) {
+    float freq = SEA_FREQ;
+    float amp = SEA_HEIGHT;
+    float choppy = SEA_CHOPPY;
+    vec2 uv = p.xz; uv.x *= 0.75;
+    
+    float d, h = 0.0;    
+    for(int i = 0; i < ITER_FRAGMENT; i++) {        
+    	d = sea_octave((uv+SEA_TIME)*freq,choppy);
+    	d += sea_octave((uv-SEA_TIME)*freq,choppy);
+        h += d * amp;        
+    	uv *= octave_m; freq *= 1.9; amp *= 0.22;
+        choppy = mix(choppy,1.0,0.2);
+    }
+    return p.y - h;
+}
+
+vec3 getSeaColor(vec3 p, vec3 n, vec3 l, vec3 eye, vec3 dist) {  
+    float fresnel = clamp(1.0 - dot(n,-eye), 0.0, 1.0);
+    fresnel = pow(fresnel,3.0) * 0.65;
+
+    vec3 reflected = getSkyColor(reflect(eye,n));    
+    vec3 refracted = SEA_BASE + diffuse(n,l,80.0) * SEA_WATER_COLOR * 0.12; 
+    
+    vec3 color = mix(refracted,reflected,fresnel);
+    
+    float atten = max(1.0 - dot(dist,dist) * 0.001, 0.0);
+    color += SEA_WATER_COLOR * (p.y - SEA_HEIGHT) * 0.18 * atten;
+    
+    color += vec3(specular(n,l,eye,60.0));
+    
+    return color;
+}
+
+// tracing
+vec3 getNormal(vec3 p, float eps) {
+    vec3 n;
+    n.y = map_detailed(p);    
+    n.x = map_detailed(vec3(p.x+eps,p.y,p.z)) - n.y;
+    n.z = map_detailed(vec3(p.x,p.y,p.z+eps)) - n.y;
+    n.y = eps;
+    return normalize(n);
+}
+
+float heightMapTracing(vec3 ori, vec3 dir, out vec3 p) {  
+    float tm = 0.0;
+    float tx = 1000.0;    
+    float hx = map(ori + dir * tx);
+    if(hx > 0.0) return tx;   
+    float hm = map(ori + dir * tm);    
+    float tmid = 0.0;
+    for(int i = 0; i < NUM_STEPS; i++) {
+        tmid = mix(tm,tx, hm/(hm-hx));                   
+        p = ori + dir * tmid;                   
+    	float hmid = map(p);
+		if(hmid < 0.0) {
+        	tx = tmid;
+            hx = hmid;
+        } else {
+            tm = tmid;
+            hm = hmid;
+        }
+    }
+    return tmid;
+}
+
+// main
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+	vec2 uv = fragCoord.xy / iResolution.xy;
+    uv = uv * 2.0 - 1.0;
+    uv.x *= iResolution.x / iResolution.y;    
+    float time = iGlobalTime * 0.3 + iMouse.x*0.01;
+        
+    // ray
+    vec3 ang = vec3(sin(time*3.0)*0.1,sin(time)*0.2+0.3,time);    
+    vec3 ori = vec3(0.0,3.5,time*5.0);
+    vec3 dir = normalize(vec3(uv.xy,-2.0)); dir.z += length(uv) * 0.15;
+    dir = normalize(dir) * fromEuler(ang);
+    
+    // tracing
+    vec3 p;
+    heightMapTracing(ori,dir,p);
+    vec3 dist = p - ori;
+    vec3 n = getNormal(p, dot(dist,dist) * EPSILON_NRM);
+    vec3 light = normalize(vec3(0.0,1.0,0.8)); 
+             
+    // color
+    vec3 color = mix(
+        getSkyColor(dir),
+        getSeaColor(p,n,light,dir,dist),
+    	pow(smoothstep(0.0,-0.05,dir.y),0.3));
+        
+    // post
+    fragColor = vec4(pow(color,vec3(0.75)), 1.0);
+}
